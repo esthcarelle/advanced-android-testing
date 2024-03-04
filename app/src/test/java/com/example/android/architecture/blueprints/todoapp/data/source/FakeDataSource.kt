db33942f1fdb2fc748b54aeede1cc2176ada5fa0
Esther Carrelle <esthcarelle@gmail.com>
2024-03-04 16:43:32 +0200

    get tasks mock implementation

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/data/source/FakeDataSource.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/data/source/FakeDataSource.kt
@@ -2,6 +2,8 @@ package com.example.android.architecture.blueprints.todoapp.data.source
 
 import androidx.lifecycle.LiveData
 import com.example.android.architecture.blueprints.todoapp.data.Result
+import com.example.android.architecture.blueprints.todoapp.data.Result.Error
+import com.example.android.architecture.blueprints.todoapp.data.Result.Success
 import com.example.android.architecture.blueprints.todoapp.data.Task
 
 class FakeDataSource(var tasks: MutableList<Task>? = mutableListOf()) : TasksDataSource  {
@@ -10,7 +12,10 @@ class FakeDataSource(var tasks: MutableList<Task>? = mutableListOf()) : TasksDat
     }
 
     override suspend fun getTasks(): Result<List<Task>> {
-
+        tasks?.let { return Result.Success(ArrayList(it)) }
+        return Error(
+            Exception("Tasks not found")
+        )
     }
 
     override suspend fun refreshTasks() {
