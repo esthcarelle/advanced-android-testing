c476893d8de8bd49636c8897d1bf9f611d8584b3
Esther Carrelle <esthcarelle@gmail.com>
2024-03-09 10:58:41 +0200

    Create fake objects

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/data/source/DefaultTasksRepositoryTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/data/source/DefaultTasksRepositoryTest.kt
@@ -1,5 +1,9 @@
 package com.example.android.architecture.blueprints.todoapp.data.source
 
+import com.example.android.architecture.blueprints.todoapp.data.Task
 import org.junit.jupiter.api.Assertions.*
 
-class DefaultTasksRepositoryTest
+class DefaultTasksRepositoryTest{
+    val task1 = Task("Title 1","Description 1")
+
+}
